c39dc04b858eb1ddd66c6b48b2df241cf2c62434
rakhm <rakhmevdanil@gmail.com>
2024-10-11 12:19:04 +0500

    Adding a category search page

--- app/src/main/java/com/example/shop_project/Activity_Search.kt
+++ app/src/main/java/com/example/shop_project/Activity_Search.kt
@@ -2,31 +2,27 @@ package com.example.shop_project
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainMenu_Activity : AppCompatActivity() {
+class Activity_Search : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_menu)
+        setContentView(R.layout.activity_search)
 
-        val imageView: ImageView = findViewById(R.id.icon_search)
+        val imageView: ImageView = findViewById(R.id.icon_main)
         imageView.setOnClickListener {
             try{
-                val intent = Intent(this, Activity_Search::class.java)
+                val intent = Intent(this, MainMenu_Activity::class.java)
                 startActivity(intent)
             }
             catch (e: Exception){
                 e.printStackTrace()
             }
         }
-
     }
 }
