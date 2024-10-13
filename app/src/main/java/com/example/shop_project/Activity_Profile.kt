f6429177509e0ae51f6f879d098a774ddd791481
rakhm <rakhmevdanil@gmail.com>
2024-10-13 21:48:36 +0500

    adding new functionality

--- app/src/main/java/com/example/shop_project/Activity_Profile.kt
+++ app/src/main/java/com/example/shop_project/Activity_Profile.kt
@@ -3,6 +3,7 @@ package com.example.shop_project
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,8 +11,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class Activity_Profile : AppCompatActivity() {
+    private lateinit var dialog: BottomSheetDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,9 +24,34 @@ class Activity_Profile : AppCompatActivity() {
         setClickListener(R.id.icon_main, MainMenu_Activity::class.java)
         setClickListener(R.id.icon_korzina, Activity_Basket::class.java)
 
-        val textView: TextView = findViewById(R.id.log_out)
-        textView.setOnClickListener {
+        val close: TextView = findViewById(R.id.log_out)
+        close.setOnClickListener {
             showLogOutDialog(MainActivity::class.java)
+        }
+
+        val order: TextView = findViewById(R.id.link_orders)
+        order.setOnClickListener {
+            showSheet(R.layout.order_sheet)
+        }
+
+        val sendMessage: TextView = findViewById(R.id.link_support)
+        sendMessage.setOnClickListener {
+            showSheet(R.layout.support_sheet)
+        }
+
+    }
+
+    private fun showSheet(imageViewId: Int) {
+        dialog = BottomSheetDialog(this)
+        dialog.setContentView(imageViewId)
+        closureSheet(R.id.icon_back)
+        dialog.show()
+    }
+
+    private fun closureSheet(imageViewId: Int){
+        val backOrder: ImageView = dialog.findViewById(imageViewId)!!
+        backOrder.setOnClickListener {
+            dialog.dismiss()
         }
     }
 
@@ -31,11 +59,11 @@ class Activity_Profile : AppCompatActivity() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle(R.string.log_out_title)
         builder.setMessage(R.string.log_out_message)
-        builder.setNegativeButton("Да") { dialog, id ->
+        builder.setNegativeButton("Да") { dialog, _ ->
             logOutAccount(targetActivity)
             dialog.dismiss()
         }
-        builder.setPositiveButton("Нет") { dialog, id ->
+        builder.setPositiveButton("Нет") { dialog, _ ->
             dialog.cancel()
         }
         val alert = builder.create()
